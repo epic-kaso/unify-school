d34f5d3fa724ec9fd53c9d02b747b3710b8c686b
Akachukwu <kasoprecede47@gmail.com>
2015-03-14 23:11:54 +0100

    badd

--- resources/views/school/register/wizard/partial/step-one.blade.php
+++ resources/views/school/register/wizard/partial/step-one.blade.php
@@ -3,13 +3,13 @@
     <hr/>
     <div class="form-group">
         <label>Enter School's Name</label>
-        <input type="text" required placeholder="School Name in Full" name="name" ng-model="school.name"
+        <input type="text" required placeholder="School Name in Full" name="name" ng-model="config.school.name"
                class="form-control inmate-search-box"/>
     </div>
 
     <div class="form-group evaluate">
         <label>Select School's Country</label>
-        <select class="form-control" required ng-model="school.country"
+        <select class="form-control" required ng-model="config.school.country"
                 ng-options="country.id as country.name for country in config.countries">
             <option value="">Select Country</option>
         </select>
@@ -17,7 +17,7 @@
 
     <div class="form-group evaluate">
         <label>Select School's State</label>
-        <select class="form-control" required ng-model="school.state">
+        <select class="form-control" required ng-model="config.school.state">
             <option value="@{{ state.id }}"
                     ng-repeat="state in config.countries[school.country - 1].states">@{{ state.name }}</option>
         </select>
@@ -25,27 +25,28 @@
 
     <div class="form-group">
         <label>Enter School's City of Residence</label>
-        <input type="text" required placeholder="Enter School's City of Residence" ng-model="school.city"
+        <input type="text" required placeholder="Enter School's City of Residence" ng-model="config.school.city"
                class="form-control inmate-search-box"/>
     </div>
 
     <div class="form-group evaluate">
         <label>Select School Type</label>
-        <select class="form-control" required ng-model="school.selected_school_type"
+        <select class="form-control" required ng-model="config.school.selected_school_type"
                 ng-options="school_type.id as school_type.display_name for school_type in config.school.school_types">
             <option value="">Select School Type</option>
         </select>
     </div>
 
-    <div class="form-group" ng-show="config.school.school_types[school.selected_school_type - 1].session != null">
+    <div class="form-group"
+         ng-show="config.school.school_types[config.school.selected_school_type - 1].session != null">
         <label>Session System</label>
         <div>
             <ul class="list-group">
                 <li class="list-group-item" style="text-transform: uppercase; font-weight: 400;">
                     @{{
-                    config.school.school_types[school.selected_school_type - 1].session.session_type + ' '+
-                    config.school.school_types[school.selected_school_type - 1].session.session_divisions_display_name+' '+
-                    config.school.school_types[school.selected_school_type - 1].session.session_display_name
+                    config.school.school_types[config.school.selected_school_type - 1].session.session_type + ' '+
+                    config.school.school_types[config.school.selected_school_type - 1].session.session_divisions_display_name+' '+
+                    config.school.school_types[config.school.selected_school_type - 1].session.session_display_name
                     }}
                     <span class="btn btn-xs btn-primary pull-right" ng-hide="onEdit"
                           ng-click="onEdit = true">Edit</span>
@@ -54,7 +55,7 @@
                         <div class="form-group">
                             <label>Session Division</label>
                             <select class="form-control" id=""
-                                    ng-model="config.school.school_types[school.selected_school_type - 1 ].session.session_type">
+                                    ng-model="config.school.school_types[config.school.selected_school_type - 1 ].session.session_type">
                                 <option value="one">One</option>
                                 <option value="two">Two</option>
                                 <option value="three">Three</option>
@@ -71,7 +72,7 @@
                         <div class="form-group">
                             <label>Session Sub-division Name</label>
                             <input type="text" class="form-control"
-                                   ng-model="config.school.school_types[school.selected_school_type - 1 ].session.session_divisions_display_name"/>
+                                   ng-model="config.school.school_types[config.school.selected_school_type - 1 ].session.session_divisions_display_name"/>
                         </div>
                         <div>
                             <span class="btn btn-info" ng-click="onEdit = false">Save</span>
@@ -83,7 +84,7 @@
         </div>
     </div>
 
-    <div class="form-group" ng-show="school.selected_school_type != ''">
+    <div class="form-group" ng-show="config.school.selected_school_type != ''">
         <div>
             <label>Select all supported school types</label>
             <ul class="list-group">
@@ -101,7 +102,7 @@
                         <div>
                                     <span class="btn btn-info"
                                           ng-click="
-                                          addCategory(school.selected_school_type - 1,school_category_name);
+                                          addCategory(config.school.selected_school_type - 1,school_category_name);
                                           onAddSchoolType = false;
                                           school_category_name = null;
                                           ">
@@ -111,11 +112,11 @@
                     </div>
                 </li>
                 <li
-                        ng-repeat="school_category in config.school.school_types[school.selected_school_type - 1].school_categories"
+                        ng-repeat="school_category in config.school.school_types[config.school.selected_school_type - 1].school_categories"
                         class="list-group-item">
                     <span class="school_category_title">@{{ school_category.display_name }}</span>
                     <span class="btn btn-xs btn-danger pull-right"
-                          ng-click="removeCategory(school.selected_school_type - 1,$index)">Remove</span>
+                          ng-click="removeCategory(config.school.selected_school_type - 1,$index)">Remove</span>
                 </li>
             </ul>
         </div>
