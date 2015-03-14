c8877b5cb68d03a9bf934b37f59078dfe6cb4874
Akachukwu <kasoprecede47@gmail.com>
2015-03-14 22:11:39 +0100

    Fixed SchoolController store bug

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -14,6 +14,12 @@ use UnifySchool\Http\Controllers\Controller;
 class AdminDashboardController extends Controller
 {
 
+
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function getIndex()
     {
         $school = $this->getSchool();
