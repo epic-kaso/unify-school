3fd5418a1f39df3254483c74549e8a4f8f7b7bb8
Akachukwu <kasoprecede47@gmail.com>
2015-03-16 20:50:23 +0100

    unify dashboard

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix
         .less('super_admin_dashboard.less', 'public/super_admin/css')
-        .less('school.less', 'public/school_admin/css/school_dashboard.css')
+        .less('school.less', 'public/school_admin/css')
         .scriptsIn('resources/js/school_admin/app', 'public/school_admin/js/main.js')
         .scriptsIn('resources/js/super_admin/app', 'public/super_admin/js/main.js')
         .scriptsIn('resources/js/app', 'public/app/js/main.js')
