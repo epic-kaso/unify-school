d7016181cc1ab6d54f32e814d36ec3fb398b9f85
Akachukwu <kasoprecede47@gmail.com>
2015-03-17 13:42:27 +0100

    sub-domain routing fixes

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix
-        .less('super_admin_dashboard.less', 'public/super_admin/css')
-        .less('school.less', 'public/school_admin/css')
+    mix.less('super_admin.less', 'public/super_admin/css');
+    mix.less('school.less', 'public/school_admin/css')
         .scriptsIn('resources/js/school_admin/app', 'public/school_admin/js/main.js')
         .scriptsIn('resources/js/super_admin/app', 'public/super_admin/js/main.js')
         .scriptsIn('resources/js/app', 'public/app/js/main.js')
