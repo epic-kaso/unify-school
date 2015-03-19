93e83f2a9ceb47b6c260283b53ba9e5f144af3b4
Akachukwu <kasoprecede47@gmail.com>
2015-03-19 02:10:02 +0100

    Slimming down & cacheing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
-<html lang="en" data-ng-app="angle">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="description" content="@{{ app.description }}">
+    <meta name="description" content="">
     <meta name="keywords" content="app, responsive, angular, bootstrap, dashboard, admin">
-    <title data-ng-bind="pageTitle()">Angle - Angular Bootstrap Admin Template</title>
-    <link rel="stylesheet" href="{{ asset('framework/app/css/app.css') }}" data-ng-if="!app.layout.isRTL">
-    <link rel="stylesheet" href="{{ asset('framework/app/css/app-rtl.css') }}" data-ng-if="app.layout.isRTL">
+    <title data-ng-bind="pageTitle()">UnifySchools</title>
+    <link rel="stylesheet" href="{{ asset('app/css/bootstrap.min.css') }}">
 </head>
 
-<body data-ng-class="{ 'layout-fixed' : app.layout.isFixed, 'aside-collapsed' : app.layout.isCollapsed, 'layout-boxed' : app.layout.isBoxed, 'layout-fs': app.useFullLayout, 'hidden-footer': app.hiddenFooter, 'layout-h': app.layout.horizontal, 'aside-float': app.layout.isFloat}">
-<div data-ui-view="" data-autoscroll="false" class="wrapper"></div>
-<script src="{{ asset('framework/app/js/base.js') }}"></script>
-<script src="{{ asset('framework/app/js/app.js') }}"></script>
+<body>
+@yield('content')
 </body>
 </html>
