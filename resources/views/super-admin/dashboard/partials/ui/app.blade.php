d6fe8d17f7e6b3adca24890187331196b28be6df
Akachukwu <kasoprecede47@gmail.com>
2015-03-20 17:08:03 +0100

    Slimming down & cacheing

--- resources/views/super-admin/dashboard/partials/ui/app.blade.php
+++ resources/views/super-admin/dashboard/partials/ui/app.blade.php
@@ -1,15 +1,15 @@
 <!-- top navbar-->
-<header ng-include="'/unify/dashboard/partial/top-navbar'" class="topnavbar-wrapper"></header>
+<header ng-include="'/unify/dashboard/partial/ui/top-navbar'" class="topnavbar-wrapper"></header>
 <!-- sidebar-->
-<aside ng-include="'/unify/dashboard/partial/sidebar'" ng-controller="SidebarController"
+<aside ng-include="'/unify/dashboard/partial/ui/sidebar'" ng-controller="SidebarController"
        class="aside"></aside>
 <!-- offsidebar-->
-<aside ng-include="'/unify/dashboard/partial/offsidebar'" class="offsidebar"></aside>
+<aside ng-include="'/unify/dashboard/partial/ui/offsidebar'" class="offsidebar"></aside>
 <!-- Main section-->
 <section>
     <!-- Page content-->
     <div ui-view="" autoscroll="false" ng-class="app.viewAnimation" class="content-wrapper"></div>
 </section>
 <!-- Page footer-->
-<footer ng-include="'/unify/dashboard/partial/footer'"></footer>
+<footer ng-include="'/unify/dashboard/partial/ui/footer'"></footer>
 <toaster-container></toaster-container>
--- resources/views/super-admin/dashboard/partials/ui/offsidebar.blade.php
+++ resources/views/super-admin/dashboard/partials/ui/offsidebar.blade.php
@@ -5,14 +5,14 @@
             <tab-heading>
                 <em class="icon-equalizer fa-lg"></em>
             </tab-heading>
-            <div ng-include="'/unify/dashboard/partial/offsidebar-tab1'"></div>
+            <div ng-include="'/unify/dashboard/partial/ui/offsidebar-tab1'"></div>
         </tab>
         <tab>
             <tab-heading>
                 <em class="icon-users fa-lg"></em>
             </tab-heading>
             <div>
-                <div ng-include="'/unify/dashboard/partial/offsidebar-tab2'"></div>
+                <div ng-include="'/unify/dashboard/partial/ui/offsidebar-tab2'"></div>
             </div>
         </tab>
     </tabset>
